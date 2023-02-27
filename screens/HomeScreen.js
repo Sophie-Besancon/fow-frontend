e61c5aedd125b31389956bd1c995a594c77e4bc4
Sophie <sobesancon@laposte.net>
2023-02-27 16:45:35 +0100

    MAJ de HomeScreen avec ajout de la recherche

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,10 @@ export default function HomeScreen({ navigation }) {
   return (
     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
         <Text>Welcome on FOW / HomeScreen </Text>
+        <TextInput placeholder="Rechercher un produit" onChangeText={(value) => setSearchProduct(value)} value={searchProduct} style={styles.input} />
+      <TouchableOpacity onPress={() => handleSubmit()} style={styles.button} activeOpacity={0.8}>
+        <Text style={styles.textButton}>Rechercher</Text>
+      </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('ContinentScreen')} style={styles.button} activeOpacity={0.8}>
       </TouchableOpacity>
     </KeyboardAvoidingView>
