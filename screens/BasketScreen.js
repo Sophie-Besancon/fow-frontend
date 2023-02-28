a8d6411db67374930804c913cd415d83876e9777
Thibault <tweb.devc@gmail.com>
2023-02-28 17:49:04 +0100

    Market Screen progress / Component Card Complete / Thibault

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -6,14 +6,48 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
+    View,
   } from 'react-native';
-  
+  import Header from '../components/Header'
+  import FontAwesome from "react-native-vector-icons/FontAwesome";
   
   export default function BasketScreen() {
   
     return (
       <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-          <Text>Welcome on FOW / BasketScreen</Text>
+                 <Header/>
+        <View style={styles.statusContainer}>
+        <FontAwesome
+            name="shopping-basket"
+            size={20}
+            color="#000000"
+            style={styles.deleteIcon}
+          />
+          <FontAwesome
+          name="user"
+          size={20}
+          color="#000000"
+          style={styles.deleteIcon}
+        />
+        <FontAwesome
+        name="money"
+        size={20}
+        color="#000000"
+        style={styles.deleteIcon}
+      />
+        </View>
+        <View style={styles.textContainer}>
+          <Text>Résumé de votre commande</Text>
+          </View>
+          <View style={styles.tableContainer}>
+            <Text>1.2.3</Text>
+          </View>
+          <View style={styles.totalContainer}>
+            <Text>4.5.6</Text>
+          </View>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Étape suivante</Text>
+          </TouchableOpacity>
       </KeyboardAvoidingView>
     )
   }
@@ -23,6 +57,42 @@ import {
       flex: 1,
       backgroundColor: '#ffffff',
       alignItems: 'center',
-      justifyContent: 'center',
+      flexDirection: 'column',
+    },
+    statusContainer:{
+      width:'50%',
+      flexDirection:'row',
+      justifyContent: 'space-around',
+      alignItems: 'space-between',
+      marginTop: 30,
+    },
+    textContainer:{
+      marginTop: 30,
+    },
+    tableContainer:{
+      marginTop: 30,
+      backgroundColor:"green",
+      width:"90%",
+      height: "35%"
+    },
+    totalContainer:{
+      marginTop: 10,
+      backgroundColor:"yellow",
+      width:"90%",
+      height:"15%",
+    },
+    buttonContainer:{
+    marginTop: 30,
+    alignSelf: "center",
+    height: 40,
+    margin: 10,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: "#4B7285",
+    width:120,
+    },
+    buttonText: {
+      alignSelf: "center",
+      color: "white",
     },
   })
--- screens/MarketScreen.js
+++ screens/MarketScreen.js
@@ -8,13 +8,30 @@ import {
     TouchableOpacity,
     View,
     SafeAreaView,
+    ScrollView,
   } from 'react-native';
 import Header from '../components/Header'
 import Card from '../components/Card'
+import { useEffect, useState } from "react";
   
   
   export default function MarketScreen() {
-  
+    const [articlesData, setarticlesData] = useState([]);
+    //console.log('articledata', articlesData.length)
+    
+    
+    useEffect(() => {
+      fetch("http://192.168.1.14:3000/articles").then((response) =>
+        response.json()).then((cardData) => {
+          setarticlesData(cardData.allArticles)
+          });
+        }, []);
+
+        const cards = articlesData.map((data, i) => {
+        console.log('data',data, i)
+        return <Card key={i} price={data.price} name={data.name} flag={data.flag} background={data.image} />;
+        });
+
     return (
 
       <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
@@ -22,8 +39,11 @@ import Card from '../components/Card'
           <View>
           <Text>Welcome on FOW / MarketScreen</Text>
           </View>
-          <Card/>
+          <ScrollView style={styles.scrollView}>
+          {cards}
+          </ScrollView>
       </KeyboardAvoidingView>
+
 
     )
   }
@@ -35,5 +55,4 @@ import Card from '../components/Card'
       alignItems: 'center',
       flexDirection: 'column',
     },
-    
   })
