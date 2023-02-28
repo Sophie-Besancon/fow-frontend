a3f326c353894195a96476bdb9488eef02196de1
Sophie <sobesancon@laposte.net>
2023-02-28 14:10:04 +0100

    création HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,22 +7,47 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  View
 } from 'react-native';
+import Header from '../components/Header';
 
 
 
 export default function HomeScreen({ navigation }) {
 
   const [searchProduct, setSearchProduct] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleSearch = () => {
+    fetch(`http://192.168.1.88:3000/articles/${searchProduct}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          setSearchResult(data.articles)
+        }
+        setSearchProduct('');
+      });
+  }
+
+  // const products = searchResult.map((product, i) => {
+  // return <View><Text>Afficher le produit: {product.name}</Text></View>
+  // })
+
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-        <Text>Welcome on FOW / HomeScreen </Text>
-        <TextInput placeholder="Rechercher un produit" onChangeText={console.log("test")} value={searchProduct} style={styles.input} />
-      <TouchableOpacity onPress={console.log("coucou")} style={styles.button} activeOpacity={0.8}>
-        <Text style={styles.textButton}>Rechercher</Text>
-      </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('ContinentScreen')} style={styles.button} activeOpacity={0.8}>
+      <Header />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Rechercher un produit" onChangeText={(value) => setSearchProduct(value)} value={searchProduct} style={styles.input} />
+        <TouchableOpacity onPress={console.log("coucou")} style={styles.inputButton} activeOpacity={0.8}>
+          <Text style={styles.textButton}>Rechercher</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity onPress={console.log("coucou2")} style={styles.continentButton} activeOpacity={0.8}>
+          <Text style={styles.textButton}>Voyager avec nous !</Text>
+        </TouchableOpacity>
+      {/* {products.length > 0 ? products : (<Text>Aucun produit trouvé</Text>)} */}
+      <TouchableOpacity onPress={() => navigation.navigate('ContinentScreen')} style={styles.button} activeOpacity={0.8}>
       </TouchableOpacity>
     </KeyboardAvoidingView>
   )
@@ -33,6 +58,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '90%',
+    backgroundColor: '#ffffff',
+    paddingLeft: 10,
+    marginTop: 10,
+    borderRadius: 8,
+    borderColor: "#4B7285",
+    borderWidth: 1,
+  },
+  inputButton: {
+    backgroundColor: '#4B7285',
+    height: 40,
+    margin: 5,
+    borderWidth: 0.2,
+    padding: 10,
+    borderRadius: 8,
+  },
+  textButton: {
+    color: '#ffffff',
+  },
+  continentButton: {
+    backgroundColor: '#FC9F30',
+    height: 40,
+    margin: 15,
+    borderWidth: 0.2,
+    padding: 10,
+    borderRadius: 8,
+    fontWeight: 900,
   },
 })
